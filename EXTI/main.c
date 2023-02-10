9c996357eb164b504c8e2bcdf686962b66054b1b
ahmedfaresabdelghani <56740338+ahmedfaresabdelghani@users.noreply.github.com>
2023-02-10 13:54:01 +0200

    updating

--- EXTI/main.c
+++ EXTI/main.c
@@ -3,11 +3,12 @@
 #include "PORT_interface.h"
 #include "GIE_interface.h"
 #include "EXTI_interface.h"
-
+void INT0_ISR(void);
 void main(void)
 {
 	PORT_voidInit();
 	EXTI_voidInt0Init();
+	EXTI_u8Int0SetCallBack(&INT0_ISR);
 	GIE_voidEnable();
 	while(1)
 	{
@@ -15,8 +16,7 @@ void main(void)
 	}
 }
 
-void __vector_1(void) __attribute__ ((signal));
-void __vector_1(void)
+void INT0_ISR(void)
 {
-	DIO_u8SetPinValue(DIO_u8PORTC,5,DIO_u8PIN_HIGH);
+	DIO_u8SetPinValue(DIO_u8PORTC,DIO_u8PIN5,DIO_u8PIN_HIGH);
 }
