24aa1a51c27c765f6986c88c2c13219e2b8c8958
ahmedfaresabdelghani <56740338+ahmedfaresabdelghani@users.noreply.github.com>
2023-02-15 20:58:27 +0200

    CLCD update

---
+++ CLCD_KPD/CLCD_interface.h
@@ -0,0 +1,33 @@
+/*************************************************/
+/*************************************************/
+/***********   Author : Ahmed Fares   ************/
+/***********   Layer : HAL            ************/
+/***********   HWC : CLCD             ************/
+/***********   Version : 1.00         ************/
+/***********   DATE : 1/28/2023       ************/
+/*************************************************/
+/*************************************************/
+
+/*Preprocessor file guard*/
+#ifndef CLCD_INTERFACE_H_
+#define CLCD_INTERFACE_H_
+
+
+void CLCD_voidSendCommand(u8 Copy_u8Command);
+
+void CLCD_voidSendData(u8 Copy_u8Data);
+
+void CLCD_voidInit(void);
+
+/*constant char because i won't change any thing in this string for any reason*/
+void CLCD_voidSendString(const char* Copy_pcString);
+
+void CLCD_voidGoToXY(u8 Copy_u8XPos,u8 Copy_u8YPos);
+
+void CLCD_voidSpecialCharacter(u8* Copy_pu8Pattern,u8 Copy_u8PatternNumber,u8 Copy_u8XPos,u8 Copy_u8YPos);
+
+
+void CLCD_PrintNumber(s32 Copy_s32Number);
+
+
+#endif
