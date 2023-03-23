ffbd87c533e8532b1c73b5e64918133034ce9fdc
Fares <Fares@192.168.1.16>
2023-03-23 23:56:06 +0200

    Component On The Shelf

--- 3-LIB/STD_TYPES.h
+++ 3-LIB/STD_TYPES.h
@@ -1,6 +1,6 @@
 #ifndef _STD_TYPES_H
 #define _STD_TYPES_H
-/************************************************/
+
 typedef unsigned char   u8;
 typedef unsigned short  u16;
 typedef unsigned int    u32;
@@ -16,5 +16,5 @@ typedef double        f64;
 #define NOK           1
 #define NULL_POINTER  2
 #define BUSY_FUNC    3
-/************************************************/
+
 #endif
